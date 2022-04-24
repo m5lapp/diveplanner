4bb8610e0d1d360e738afdf0ad68dcabdae13a70
Michael Appleyard <mijoap@gmail.com>
2022-04-24 23:26:06 +0100

    Done a lot of work on the dive planner and added a few tests.

--- diveplanner.go
+++ diveplanner.go
@@ -10,16 +10,24 @@ import (
 )
 
 const (
+	// Common/useful dive factor multipliers.
+	DiveFactorEasy          float64 = 1.5
+	DiveFactorModerate      float64 = 1.8
+	DiveFactorTough         float64 = 2.0
+	DiveFactorStressful     float64 = 2.5
+	DiveFactorSeriousStress float64 = 3.0
+
 	otuRepetitiveDiveLimit float64 = 300.0
 	otuSingleDiveLimit     float64 = 850.0
-	safetyStopDepth        float64 = 5.0
+
+	safetyStopDepth float64 = 5.0
 )
 
 type DivePlanStop struct {
-	Stop     int     `bson:"stop" json:"stop"`
-	Depth    float64 `bson:"depth" json:"depth"`
-	Duration float64 `bson:"duration" json:"duration"`
-	Comment  string  `bson:"comment" json:"comment"`
+	Depth        float64 `bson:"depth" json:"depth"`
+	Duration     float64 `bson:"duration" json:"duration"`
+	IsTransition bool    `bson:"is_transition" json:"is_transition"`
+	Comment      string  `bson:"comment" json:"comment"`
 }
 
 // GasRequirement() calculates the amount of breathing gas that a diver with a
@@ -41,29 +49,77 @@ type DivePlan struct {
 	TankCount       int             `bson:"tank_count" json:"tank_count"`
 	TankCapacity    float64         `bson:"tank_capacity" json:"tank_capacity"`
 	WorkingPressure int             `bson:"working_pressure" json:"working_pressure"`
-	NixtroxMix      gasmix.GasMix   `bson:"nitrox_mix" json:"nitrox_mix"`
+	DiveFactor      float64         `bson:"dive_factor" json:"dive_factor"`
+	GasMix          *gasmix.GasMix  `bson:"nitrox_mix" json:"nitrox_mix"`
 	MaxPPO2         float64         `bson:"max_ppo2" json:"max_ppo2"`
 	Stops           []*DivePlanStop `bson:"stops" json:"stops"`
 }
 
 // transitionDuration() calculates the amount of time in minutes required to
-// transition from depth d1 in metres to depth d2 in metres at the configured
-// ascent or descent rate.
-func (dp *DivePlan) transitionDuration(d1, d2 float64) (float64, error) {
-	depthDelta := d2 - d1
-	if depthDelta == 0.0 {
-		return 0.0, fmt.Errorf("calcTransition: no delta between the depths "+
-			"provided: d1: %f, d2: %f.", d1, d2)
-	} else if depthDelta > 0.0 {
-		// The depth delta is positive which means we are descending.
-		return depthDelta / dp.DescentRate, nil
-	} else {
+// transition from one depth in metres to another at the configured ascent or
+// descent rate, rounded up to the nearest minute for conservatism. If the
+// difference between the two depths is less than half a metre, then that is not
+// considered a transition and zero is returned.
+func (dp *DivePlan) transitionDuration(fromD, toD float64) float64 {
+	var time float64 = 0.0
+	depthDelta := toD - fromD
+
+	// If the depthDelta is less than half a metre either way, then time remains
+	// set to zero as it's not worth spending a minute to get there.
+	if depthDelta >= 0.5 {
+		// The depth delta is positive (or zero) which means we are descending.
+		time = math.Abs(depthDelta / dp.DescentRate)
+	} else if depthDelta <= -0.5 {
 		// The depth delta is negative which means we are ascending.
-		return depthDelta / dp.AscentRate, nil
+		time = math.Abs(depthDelta / dp.AscentRate)
+	}
+
+	// For conservatism, round the time up to the nearest minute.
+	return math.Ceil(time)
+}
+
+// transitionStop() returns a DivePlanStop that represents a transition from one
+// depth to another; it's Depth value is the average of the two.
+func (dp *DivePlan) transitionStop(fromD, toD float64) *DivePlanStop {
+	dir := "Descent"
+	if toD < fromD {
+		dir = "Ascent"
+	}
+
+	return &DivePlanStop{
+		Duration:     dp.transitionDuration(fromD, toD),
+		Depth:        math.Abs((fromD + toD) / 2.0),
+		IsTransition: true,
+		Comment:      fmt.Sprintf("%s from %.1fm to %.1fm", dir, fromD, toD),
 	}
 }
 
-// MaxDepth() simply returns the depth at the deepest point of the dive plan.
+// calcTransitions() updates the dp.Stops slice with the transition to each stop
+// from the previous one. Finally it will include the transition from the last
+// stop back to the surface.
+func (dp *DivePlan) calcTransitions() {
+	var currDepth float64
+	var stops []*DivePlanStop
+
+	for _, s := range dp.Stops {
+		// Check that the stop is a stop and not a previously calculated
+		// transition.
+		if !s.IsTransition && s.Depth > 0.0 && s.Duration > 0.0 {
+			t := dp.transitionStop(currDepth, s.Depth)
+			stops = append(stops, t, s)
+			currDepth = s.Depth
+		}
+	}
+
+	// Include the final transition back to the surface and update dp.Stops.
+	if len(stops) > 0 {
+		t := dp.transitionStop(currDepth, 0.0)
+		dp.Stops = append(stops, t)
+	}
+}
+
+// MaxDepth() returns the depth at the deepest point of the dive plan or zero if
+// there are no stops in the plan.
 func (dp *DivePlan) MaxDepth() float64 {
 	maxDepth := 0.0
 	for _, s := range dp.Stops {
@@ -74,42 +130,58 @@ func (dp *DivePlan) MaxDepth() float64 {
 	return maxDepth
 }
 
-// Runtime() simply sums the duration of each stop in the plan and returns that
-// value in minutes.
+// Runtime() sums the duration of each stop in the plan plus the ascent and
+// descent times and returns that value in minutes.
 func (dp *DivePlan) Runtime() float64 {
-	runtime := 0.0
+	var currDepth, runtime float64
+
+	// For each stop in the plan, get the duration of the transition to that
+	// stop, plus the stop itself.
 	for _, s := range dp.Stops {
-		runtime += s.Duration
+		td := dp.transitionDuration(currDepth, s.Depth)
+		runtime += td + s.Duration
+		currDepth = s.Depth
 	}
+
+	// Include the final transition back to the surface.
+	runtime += dp.transitionDuration(currDepth, 0.0)
 	return runtime
 }
 
 // Pulmonary Oxygen Toxicity calculates the number of Oxygen Tolerence Units
-// (OTU) for the dive. One OTU is equivalent to breathing 100% Oxygen at 1 bar
+// (OTUs) for the dive. One OTU is equivalent to breathing 100% Oxygen at 1 bar
 // for 1 minute. The single dive limit is 850 OTU on day 1 and 300 OTU for
 // repetitive dives on day 2+.
-func (dp *DivePlan) POT() (otu float64) {
-	otu = 0.0
+func (dp *DivePlan) POT() float64 {
+	var currDepth, otu float64
+
+	// For each stop in the plan, get the duration of the transition to that
+	// stop, plus the stop itself.
 	for _, s := range dp.Stops {
-		ppo2 := dp.NixtroxMix.PPO2(s.Depth)
-		otu += ppo2 * float64(s.Duration)
+		t := dp.transitionStop(currDepth, s.Depth)
+		otu += dp.GasMix.PPO2(t.Depth) * t.Duration
+		otu += dp.GasMix.PPO2(s.Depth) * s.Duration
+		currDepth = s.Depth
 	}
+
+	// Include the final transition back to the surface.
+	t := dp.transitionStop(currDepth, 0.0)
+	otu += dp.GasMix.PPO2(t.Depth) * t.Duration
 	return otu
 }
 
 // MinGas() returns the amount of gas required to get two divers (or one if
 // diving solo) to the surface in an emergency from the deepest part of the dive
-// with a safety stop.
+// with a safety stop. For solo dives, the minimum gas is still doubled as it is
+// required to be available from two independent gas sources.
 func (dp *DivePlan) MinGas() float64 {
 	maxDepth := dp.MaxDepth()
 	maxPressure := helpers.Pressure(maxDepth)
 	avgPressure := helpers.Pressure(maxDepth / 2.0)
 	stopPressure := helpers.Pressure(safetyStopDepth)
-	ascentTime := maxDepth / dp.AscentRate
-	buddyMultiplier := 2.0
-	if dp.IsSoloDive {
-		buddyMultiplier = 1.0
-	}
+	ascentTime := dp.transitionDuration(maxDepth, 0.0)
+	const buddyMultiplier float64 = 2.0
+
 	// Account for elevated breathing rate in an emergency.
 	elevatedSACRate := dp.SACRate * buddyMultiplier * 1.5
 
@@ -126,8 +198,8 @@ func (dp *DivePlan) MinGas() float64 {
 	return preperationGas + ascentGas + stopGas
 }
 
-// GasAvailable returns the total amount of gas available to the diver with the
-// equipment configuration specified.
+// GasAvailable() returns the total amount of gas available to the diver with
+// the equipment configuration specified.
 func (dp *DivePlan) GasAvailable() float64 {
 	return float64(dp.TankCount) * dp.TankCapacity * float64(dp.WorkingPressure)
 }
@@ -140,12 +212,23 @@ func (dp *DivePlan) WorkingGas() float64 {
 
 // baseGasRequired() calculates the amount of gas required for the dive as
 // planned; the descent, the ascent and each stop. It does not include any
-// contingency and so should not be used without applying the rule of thirds.
+// contingency and so should not be used without using additonal gas planning.
 func (dp *DivePlan) baseGasRequired() float64 {
-	gasRequired := 0.0
+	var currDepth, gasRequired float64
+
+	// For each stop, first calculate the gas required to transition to that
+	// stop (using the mean depth), then the gas required for the stop.
 	for _, s := range dp.Stops {
+		t := dp.transitionStop(currDepth, s.Depth)
+		gasRequired += t.GasRequirement(dp.SACRate)
 		gasRequired += s.GasRequirement(dp.SACRate)
+		currDepth = s.Depth
 	}
+
+	// Finally, add the gas for the transition from the last stop to the surface.
+	t := dp.transitionStop(currDepth, 0.0)
+	gasRequired += t.GasRequirement(dp.SACRate)
+
 	return gasRequired
 }
 
@@ -183,7 +266,7 @@ func (dp *DivePlan) IsSawToothProfile() bool {
 func (dp *DivePlan) DiveIsPossible() bool {
 	isSawTooth := dp.IsSawToothProfile()
 	sufficientGas := dp.GasSpare() >= 0.0
-	withinMOD := dp.MaxDepth() <= dp.NixtroxMix.MOD(dp.MaxPPO2)
+	withinMOD := dp.MaxDepth() <= dp.GasMix.MOD(dp.MaxPPO2)
 	return !isSawTooth && sufficientGas && withinMOD
 }
 
@@ -219,7 +302,13 @@ func (dp *DivePlan) walkTransition(currDepth, targetDepth float64,
 	// positive value means descending, negative is ascending.
 	depthDelta := targetDepth - currDepth
 	// The amount of time in seconds it will take to transfer to the next stop.
-	transitionTime := math.Abs(depthDelta / dp.DescentRate * 60.0)
+	transitionTime := dp.transitionDuration(currDepth, targetDepth)
+	if transitionTime == 0.0 {
+		// The delta between the two depths is not large enough to be considered
+		// a proper transition.
+		return currTime, currDepth
+	}
+
 	// The number of samples to get to the next stop.
 	samples := transitionTime / float64(res)
 	// The difference in depth between each consecutive sample.
@@ -246,15 +335,13 @@ func main() {
 		TankCount:       2,
 		TankCapacity:    11.0,
 		WorkingPressure: 200,
-		NixtroxMix:      gasmix.GasMix{FO2: 0.32},
+		GasMix:          &gasmix.GasMix{FO2: 0.32},
 		MaxPPO2:         1.4,
 		Stops: []*DivePlanStop{
-			//{1, 12.5, 3, "Descent, depth is average"},
-			{2, 25.0, 13, ""},
-			{3, 18.0, 15, ""},
-			{4, 12.0, 23, ""},
-			{5, 5.0, 3, ""},
-			//{6, 12.5, 3, "Ascent, depth is average"},
+			{25.0, 13, false, ""},
+			{18.0, 15, false, ""},
+			{12.0, 23, false, ""},
+			{5.0, 3, false, "Safety stop"},
 		},
 	}
 
--- diveplanner_test.go
+++ diveplanner_test.go
@@ -1,1 +1,158 @@
 package diveplanner
+
+import "testing"
+
+func TestTransitionDuration(t *testing.T) {
+	tests := []struct {
+		name  string
+		dp    *DivePlan
+		fromD float64
+		toD   float64
+		want  float64
+	}{
+		{
+			name:  "Desc 0m to 27m",
+			dp:    &DivePlan{DescentRate: 20},
+			fromD: 0.0,
+			toD:   27.0,
+			want:  2.0,
+		},
+		{
+			name:  "Desc 12m to 18m",
+			dp:    &DivePlan{DescentRate: 9},
+			fromD: 12.0,
+			toD:   18.0,
+			want:  1.0,
+		},
+		{
+			name:  "Asc 16m to 5m",
+			dp:    &DivePlan{AscentRate: 10},
+			fromD: 16.0,
+			toD:   5.0,
+			want:  2.0,
+		},
+		{
+			name:  "Tiny delta",
+			dp:    &DivePlan{DescentRate: 10},
+			fromD: 10.00000000000000000000,
+			toD:   10.00000000000000100001,
+			want:  0.0,
+		},
+		{
+			name:  "No depth delta",
+			dp:    &DivePlan{DescentRate: 10},
+			fromD: 5.0,
+			toD:   5.0,
+			want:  0.0,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			time := tt.dp.transitionDuration(tt.fromD, tt.toD)
+			if time != tt.want {
+				t.Errorf("want: %f; got: %f", tt.want, time)
+			}
+		})
+	}
+}
+
+func TestCalcTransition(t *testing.T) {
+	tests := []struct {
+		name string
+		dp   *DivePlan
+		want []*DivePlanStop
+	}{
+		{
+			name: "No stops",
+			dp: &DivePlan{
+				DescentRate: 20,
+				AscentRate:  9,
+				Stops:       []*DivePlanStop{},
+			},
+			want: []*DivePlanStop{},
+		},
+		{
+			name: "22m bounce dive",
+			dp: &DivePlan{
+				DescentRate: 20,
+				AscentRate:  9,
+				Stops: []*DivePlanStop{
+					{22.0, 26, false, ""},
+					{5.0, 3, false, ""},
+				},
+			},
+			want: []*DivePlanStop{
+				{11.0, 2, true, "Descent from 0.0m to 22.0m"},
+				{22.0, 26, false, ""},
+				{13.5, 2, true, "Ascent from 22.0m to 5.0m"},
+				{5.0, 3, false, ""},
+				{2.5, 1, true, "Ascent from 5.0m to 0.0m"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.dp.calcTransitions()
+			for i, s := range tt.dp.Stops {
+				if s.Depth != tt.want[i].Depth ||
+					s.Duration != tt.want[i].Duration ||
+					s.IsTransition != tt.want[i].IsTransition ||
+					s.Comment != tt.want[i].Comment {
+					t.Errorf("want: %v; got: %v", tt.want, s)
+				}
+			}
+		})
+	}
+}
+
+func TestRuntime(t *testing.T) {
+	tests := []struct {
+		name string
+		dp   *DivePlan
+		want float64
+	}{
+		{
+			name: "No stops",
+			dp: &DivePlan{
+				DescentRate: 20,
+				AscentRate:  9,
+				Stops:       []*DivePlanStop{},
+			},
+			want: 0.0,
+		}, {
+			name: "61min dive",
+			dp: &DivePlan{
+				DescentRate: 20,
+				AscentRate:  9,
+				Stops: []*DivePlanStop{
+					{25.0, 13, false, ""},
+					{18.0, 15, false, ""},
+					{12.0, 23, false, ""},
+					{5.0, 3, false, ""},
+				},
+			},
+			want: 2 + 13 + 1 + 15 + 1 + 23 + 1 + 3 + 1,
+		}, {
+			name: "Bounce dive",
+			dp: &DivePlan{
+				DescentRate: 18,
+				AscentRate:  6,
+				Stops: []*DivePlanStop{
+					{40.0, 1, false, ""},
+				},
+			},
+			want: 3 + 1 + 7,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rt := tt.dp.Runtime()
+			if rt != tt.want {
+				t.Errorf("want: %f; got: %f", tt.want, rt)
+			}
+		})
+	}
+}
